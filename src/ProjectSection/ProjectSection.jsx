1d156773f05b358c379c8f869c5a53aad30ce324
David2008-coder <obasukedavid47@gmail.com>
2025-11-20 06:39:32 -0800

    Initial commit

--- src/ProjectSection/ProjectSection.jsx
+++ src/ProjectSection/ProjectSection.jsx
@@ -9,14 +9,14 @@ const ProjectSection = () => {
         <div className="individual-project">
           <h3 className="project-name">Project 1</h3>
           <p className="project-description">Project 1 description</p>
-          <a href="#" target="_blank" rel="" className="project-link">Live Demo</a>
-          <a href="#" target="_blank" rel="https://real-estate-psi-bice.vercel.app/" className="project-link">GitHub Repo</a>
+          <a href="https://real-estate-psi-bice.vercel.app/" target="_blank" rel="" className="project-link">Live Demo</a>
+          <a href="https://github.com/David2008-coder/Real-Estate-" target="_blank" rel="" className="project-link">GitHub Repo</a>
         </div>
         <div className="individual-project">
           <h3 className="project-name">Project 2</h3>
           <p className="project-description">Project 2 description</p>
-          <a href="#" target="_blank" rel="https://wristwatchwebsite.vercel.app/" className="project-link">Live Demo</a>
-          <a href="#" target="_blank" rel="https://petwebsite-qx7d.vercel.app/" className="project-link">GitHub Repo</a>
+          <a href="https://bagwebsitetwo.vercel.app/" target="_blank" rel="" className="project-link">Live Demo</a>
+          <a href="https://github.com/David2008-coder/bagwebsitetwo" target="_blank" rel="" className="project-link">GitHub Repo</a>
         </div>
       </div>
       <h2 className="section-title">Collaborated Projects</h2>
@@ -24,14 +24,14 @@ const ProjectSection = () => {
         <div className="individual-project">
           <h3 className="project-name">Collaborated Project 1</h3>
           <p className="project-description">Collaborated Project 1 description</p>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="project-link">Live Demo</a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="project-link">GitHub Repo</a>
+          <a href="https://wristwatchwebsite.vercel.app/" target="_blank" rel="noopener noreferrer" className="project-link">Live Demo</a>
+          <a href="https://github.com/David2008-coder/wristwatchwebsite" target="_blank" rel="noopener noreferrer" className="project-link">GitHub Repo</a>
         </div>
         <div className="individual-project">
           <h3 className="project-name">Collaborated Project 2</h3>
           <p className="project-description">Collaborated Project 2 description</p>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="project-link">Live Demo</a>
-          <a href="#" target="_blank" rel="noopener noreferrer" className="project-link">GitHub Repo</a>
+          <a href="https://petwebsite-qx7d.vercel.app/" target="_blank" rel="noopener noreferrer" className="project-link">Live Demo</a>
+          <a href="https://github.com/David2008-coder/petwebsite" target="_blank" rel="noopener noreferrer" className="project-link">GitHub Repo</a>
         </div>
       </div>
     </section>
